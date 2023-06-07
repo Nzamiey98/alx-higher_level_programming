9443d5a8acb368f13481ece3491866a6d99d9c5d
Nzamiey@98 <Nondzamekonosisi@gmail.com>
2023-06-07 03:45:37 -0700

    updated

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# 8-rectangle.py
 """Defines a Rectangle class."""
 
 
 class Rectangle:
     """Represent a rectangle.
+
     Attributes:
-    number_of_instances (int): The number of Rectangle instances.
-    print_symbol (any): The symbol used for string representation.
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
     """
 
     number_of_instances = 0
@@ -15,9 +15,10 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+
         Args:
-        width (int): The width of the new rectangle.
-        height (int): The height of the new rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
         self.width = width
@@ -62,11 +63,12 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the Rectangle with the greater area.
-        Arga:
-        rect_1 (Rectangle): The first Rectangle.
-        rect_2 (Rectangle): The second Rectangle.
+
+        Args:
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
         Raises:
-        TypeError: If either of rect_1 or rect_2 is not a Rectangle.
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -78,18 +80,19 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
+
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
-        
+
         rect = []
         for i in range(self.__height):
             [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-                return ("".join(rect))
-    
+        return ("".join(rect))
+
     def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
