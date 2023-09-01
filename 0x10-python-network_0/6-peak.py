dfd0de699cad49c6ef7ebfa0a5c77bbd325a8570
Nzamiey@98 <Nondzamekonosisi@gmail.com>
2023-08-31 17:52:49 -0700

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,15 +11,15 @@ def find_peak(numbr):
     length = len(numbr)
     if length == 0:
         return None
-    if lenght == 1:
+    if length == 1:
         return (numbr[0])
-    if lenght == 2:
+    if length == 2:
         return numbr[0] if numbr[0] >= numbr[1] else numbr [1]
 
     for idx in range(0, length):
         value = numbr[idx]
         if (idx > 0 and idx < length - 1 and 
-                numbr[idx + 1] <= vvalue and numbr[idx - 1] <= value):
+                numbr[idx + 1] <= value and numbr[idx - 1] <= value):
             return value
         elif idx == 0 and numbr[idx + 1] <= value:
             return value
