57f355bfb8d33813cddf30ee3e425118a811592d
Nzamiey98 <124698373+Nzamiey98@users.noreply.github.com>
2023-11-13 12:55:37 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const callMeMoby = (x, theFunction) => {
-	  if (x > 0) {
-		      theFunction();
-		      callMeMoby(x - 1, theFunction);
-		    }
-};
-module.exports.callMeMoby = callMeMoby;
-
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const addMeMaybe = (number, theFunction) => {
-	  number++;
-	  theFunction(number);
-};
-module.exports.addMeMaybe = addMeMaybe;
-
